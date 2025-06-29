f95659feb21676f19daf08ef625ac214b5f993ff
IAN <ingenieroiansuarez@gmail.com>
2025-06-29 01:28:39 -0500

    add flash message for successful form submission and fix radio button labels in job application form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request ,flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -35,6 +35,7 @@ def index():
                     occupation=occupation)
         db.session.add(form)
         db.session.commit()
+        flash("Form submitted successfully!", "success")
 
 
     return render_template("index.html")
