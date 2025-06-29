18f756b83957ae5cb070d549b13b0633f2029b47
IAN <ingenieroiansuarez@gmail.com>
2025-06-29 02:32:39 -0500

    add email notification for form submissions and configure Flask-Mail

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template, request ,flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "mysecretkey"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = "ingenieroiansuarez@gmail.com"
+app.config["MAIL_PASSWORD"] = "dazk okhr itoy jvkg"
+
 db = SQLAlchemy(app)
+
+mail = Mail(app)
 
 class Form(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -35,6 +44,18 @@ def index():
                     occupation=occupation)
         db.session.add(form)
         db.session.commit()
+
+        # Send email notification
+        message_body = f"thank you for your submission, {first_name}." \
+                       f"Here are the details:\n{first_name}\n{last_name}\n{date}\n" \
+                       f"Thank you for your submission, {first_name}.\n"
+
+        message = Message(subject="New Form Submission",
+                          sender=app.config["MAIL_USERNAME"],
+                          recipients=[email],
+                          body=message_body)
+        mail.send(message)
+
         flash("Form submitted successfully!", "success")
 
 
